1226cad8b94bc0f6b8b27d83d6b40790397d3689
git-chetan91 <97801289+git-chetan91@users.noreply.github.com>
2022-01-16 00:30:54 +0530

    Create hform-admin-display.php

---
+++ admin/partials/hform-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       ca_hform
+ * @since      1.0.0
+ *
+ * @package    Hform
+ * @subpackage Hform/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
