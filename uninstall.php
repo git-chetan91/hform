dae607bfd12bd9afec2c384f6cf8a2e7d3669951
git-chetan91 <97801289+git-chetan91@users.noreply.github.com>
2022-01-15 23:28:20 +0530

    My plugin files

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ *
+ * @link       ca_hform
+ * @since      1.0.0
+ *
+ * @package    Hform
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
